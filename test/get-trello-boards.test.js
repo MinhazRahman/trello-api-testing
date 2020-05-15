0bd9a9a31cb78460232521c516bce997aacf8cf6
Mohammed Rahman <minhaz357@gmail.com>
2020-05-15 15:43:12 -0400

    Can get a board by ID

---
+++ test/get-trello-boards.test.js
@@ -0,0 +1,54 @@
+const {
+  expect,
+} = require('chai');
+
+const TrelloBoardsApi = require('../src/TrelloBoardsApi');
+const randomString = require('../util/random-string');
+
+describe('Get board by ID', () => {
+  let trelloBoardsApi;
+  let boards = [];
+
+  // runs before the first test
+  before(async () => {
+    trelloBoardsApi = new TrelloBoardsApi();
+    // user authentication by providing api key and token
+    trelloBoardsApi.authenticate();
+  });
+
+  // runs after every tests
+  afterEach(async () => {
+    // create an array of deleteBoard promises
+    const arrayOfDeleteBoardPromises = boards.map(async (board) => {
+      await trelloBoardsApi.deleteBoard(board.id);
+    });
+      // delete all the created boards
+    await Promise.all(arrayOfDeleteBoardPromises);
+    console.log(`DELETED ${boards.length} BOARDS`);
+    // reset the created boards
+    boards = [];
+  });
+
+  it('Can get a board by ID (GET /1/boards/:id)', async () => {
+    // set query strings
+    const queryParameters = {
+      name: `${randomString()}`,
+      desc: `${randomString()}`,
+      defaultLabels: false,
+    };
+
+    // create board by passing only required parameter 'name'
+    const boardCreated = await trelloBoardsApi.createBoard(queryParameters);
+
+    // store the created board to boards array for cleaning up after each test
+    boards.push(boardCreated);
+
+    // get a board by id
+    const boardRetrieved = await trelloBoardsApi.getBoard(boardCreated.id);
+
+    // verify values from response
+    expect(boardRetrieved).to.have.property('id').is.a('string');
+    expect(boardRetrieved).to.have.property('name', queryParameters.name);
+    expect(boardRetrieved).to.have.property('desc', queryParameters.desc);
+  });
+});
