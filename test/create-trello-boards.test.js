26f2787ecaa98a12e8e87a1fae1395cf46d90ed0
Mohammed Rahman <minhaz357@gmail.com>
2020-05-15 03:01:19 -0400

    Can create a new board with different length of the "name" property

--- test/create-trello-boards.test.js
+++ test/create-trello-boards.test.js
@@ -1,9 +1,8 @@
-const {
-  expect,
-} = require('chai');
+const { expect } = require('chai');
 
 const TrelloBoardsApi = require('../src/TrelloBoardsApi');
 const randomString = require('../util/random-string');
+const repeatCharacter = require('../util/repeat-character');
 
 describe('Create a board', () => {
   let trelloBoardsApi;
@@ -45,7 +44,7 @@ describe('Create a board', () => {
     expect(boardCreated.prefs).to.property('cardAging', 'regular');
   });
 
-  it.only('Can create a new board with optional parameters (POST /1/boards/)', async () => {
+  it('Can create a new board with optional parameters (POST /1/boards/)', async () => {
     // set query strings
     const queryStrings = {
       name: `${randomString()}`,
@@ -54,8 +53,6 @@ describe('Create a board', () => {
     };
     // create board by passing only required parameter 'name'
     const boardCreated = await trelloBoardsApi.createBoard(queryStrings);
-
-    console.log(boardCreated);
 
     // verify values from the response
     expect(boardCreated).to.have.property('id').to.be.a('string');
@@ -74,5 +71,38 @@ describe('Create a board', () => {
     expect(boardCreated.prefs).to.property('cardCovers', true);
     expect(boardCreated.prefs).to.property('background', 'blue');
     expect(boardCreated.prefs).to.property('cardAging', 'regular');
+  });
+
+  // names array contains strings of 1, 2 and 16384 characters (works up 7596 characters)
+  const names = [repeatCharacter('X', 1), repeatCharacter('Y', 2), repeatCharacter('Z', 7596)];
+  names.forEach((name) => {
+    it.only(`Can create a new board with different length of the "name" property (POST /1/boards/) (DD), length: ${name.length}`, async () => {
+      // set query strings
+      const queryStrings = {
+        name: name,
+        desc: `${randomString()}`,
+        defaultLabels: false,
+      };
+      // create board by passing only required parameter 'name'
+      const boardCreated = await trelloBoardsApi.createBoard(queryStrings);
+      // verify values from the response
+      expect(boardCreated).to.have.property('id').to.be.a('string');
+      expect(boardCreated).to.have.property('name', queryStrings.name);
+      expect(boardCreated.name).to.have.lengthOf(queryStrings.name.length);
+      expect(boardCreated).to.have.property('desc', queryStrings.desc);
+      expect(boardCreated, 'descData should be null').to.have.property('descData', null);
+      expect(boardCreated).to.have.property('closed', false);
+      expect(boardCreated).to.have.property('idOrganization', null);
+      expect(boardCreated).to.have.property('idEnterprise', null);
+
+      expect(boardCreated.prefs).to.property('permissionLevel', 'private');
+      expect(boardCreated.prefs).to.property('voting', 'disabled');
+      expect(boardCreated.prefs).to.property('comments', 'members');
+      expect(boardCreated.prefs).to.property('invitations', 'members');
+      expect(boardCreated.prefs).to.property('selfJoin', true);
+      expect(boardCreated.prefs).to.property('cardCovers', true);
+      expect(boardCreated.prefs).to.property('background', 'blue');
+      expect(boardCreated.prefs).to.property('cardAging', 'regular');
+    });
   });
 });
---
+++ util/repeat-character.js
@@ -0,0 +1,7 @@
+// function to generate a string of length n
+function repeatCharacter(char, count) {
+  return char.repeat(count);
+}
+
+// export the repeatCharacter function
+module.exports = repeatCharacter;
