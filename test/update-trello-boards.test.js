538ed82f125337d9492e750b659c00bbd845ae88
Mohammed Rahman <minhaz357@gmail.com>
2020-05-15 15:11:48 -0400

    Can update multiple properties of a board by id

--- test/update-trello-boards.test.js
+++ test/update-trello-boards.test.js
@@ -51,4 +51,29 @@ describe('Update a board', () => {
     expect(updatedBoard).to.have.property('name', updatedQueryParameters.name);
     expect(updatedBoard).to.have.property('desc', boardCreated.desc);
   });
+
+  it.only('Can update multiple properties of a board by id (PUT /1/boards/:id)', async () => {
+    // set query strings
+    const queryParameters = {
+      name: `${randomString()}`,
+      desc: `${randomString()}`,
+      defaultLabels: false,
+    };
+
+    // create board by passing only required parameter 'name'
+    const boardCreated = await trelloBoardsApi.createBoard(queryParameters);
+
+    // store the created board to boards array for cleaning up after each test
+    boards.push(boardCreated);
+
+    // set different set of query parameters to update the board, (update name and description)
+    const updatedQueryParameters = { ...queryParameters, name: `${randomString()}`, desc: `${randomString()}` };
+    // update the board
+    const updatedBoard = await trelloBoardsApi.updateBoard(boardCreated.id, updatedQueryParameters);
+
+    // verify values from response
+    expect(updatedBoard).to.have.property('id', boardCreated.id);
+    expect(updatedBoard).to.have.property('name', updatedQueryParameters.name);
+    expect(updatedBoard).to.have.property('desc', updatedQueryParameters.desc);
+  });
 });
